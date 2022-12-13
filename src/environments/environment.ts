8256e28e0459235dede2494f1ea53e888ee72482
Christian Vega <hasergecris9@gmail.com>
2022-12-13 16:23:16 -0500

    actaulizacion

--- src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-
-
-export const environment = {
-  production: false,
-  BASE_URL: 'https://thecatapi.com/',
-  API_KEY: 'live_wtWZNpJQmCcv8zYsMpVyGgl0yIkQ2EmaKkjC65UDtuvAcpKod1bK8eIPuQsH7dvl'
-};
-
-
